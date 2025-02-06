2ba294a868d57f506af62a324aa7c68f0ce02fa5
jatin-fs <jatin.yadav+github@fxis.ai>
2025-02-06 18:14:03 +0530

    Add brand

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware('auth',AuthAdmin::class)->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
+    Route::get('/admin/brands/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
+    Route::post('/admin/brands/store', [AdminController::class, 'brand_store'])->name('admin.brand.store');
+});
+
+Route::get('/home',function (){
+    return redirect()->route('home.index');
+});
