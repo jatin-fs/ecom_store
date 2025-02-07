3370d81b43f4857c43d2a6a569626b3374b15f77
jatin-fs <jatin.yadav+github@fxis.ai>
2025-02-07 11:31:40 +0530

    Edit Brand

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,10 @@ Route::middleware('auth')->group(function () {
 Route::middleware('auth',AuthAdmin::class)->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
-    Route::get('/admin/brands/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
-    Route::post('/admin/brands/store', [AdminController::class, 'brand_store'])->name('admin.brand.store');
+    Route::get('/admin/brand/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
+    Route::post('/admin/brand/store', [AdminController::class, 'brand_store'])->name('admin.brand.store');
+    Route::get('/admin/brand/edit/{id}', [AdminController::class, 'edit_brand'])->name('admin.brand.edit');
+    Route::put('/admin/brand/update', [AdminController::class, 'update_brand'])->name('admin.brand.update');
 });
 
 Route::get('/home',function (){
