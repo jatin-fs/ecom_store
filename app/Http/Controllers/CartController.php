f384dca804dfc041dc338f72cfe3cfad0c90c2ff
jatin-fs <jatin.yadav+github@fxis.ai>
2025-02-13 15:30:45 +0530

    Checkout COD

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use App\Models\Coupon;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Transaction;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Surfsidemedia\Shoppingcart\Facades\Cart;
 
 class CartController extends Controller
@@ -107,5 +113,129 @@ class CartController extends Controller
         session()->forget('coupon');
         session()->forget('discounts');
         return back()->with('success', 'Coupon has been removed!');
+    }
+
+    public function checkout()
+    {
+        if (!Auth::check()) {
+            return redirect()->route("login");
+        }
+        $address = Address::where('user_id', Auth::user()->id)->where('isdefault', 1)->first();
+        return view('checkout', compact("address"));
+    }
+
+    public function place_order(Request $request)
+    {
+        $user_id = Auth::user()->id;
+
+        $address = Address::where('user_id', Auth::user()->id)->where('isdefault', 1)->first();
+        if (!$address) {
+            $request->validate([
+                'name' => 'required|max:100',
+                'phone' => 'required|numeric|digits:10',
+                'zip' => 'required|numeric|digits:6',
+                'state' => 'required',
+                'city' => 'required',
+                'address' => 'required',
+                'locality' => 'required',
+                'landmark' => 'required'
+            ]);
+
+            $address = new Address();
+            $address->user_id = $user_id;
+            $address->name = $request->name;
+            $address->phone = $request->phone;
+            $address->zip = $request->zip;
+            $address->state = $request->state;
+            $address->city = $request->city;
+            $address->address = $request->address;
+            $address->locality = $request->locality;
+            $address->landmark = $request->landmark;
+            $address->country = 'India';
+            $address->isdefault = true;
+            $address->save();
+        }
+
+        $this->setAmountForCheckout();
+
+        $order = new Order();
+        $order->user_id = $user_id;
+        $order->subtotal =  floatval(str_replace(',', '', Session::get('checkout')['subtotal']));
+        $order->discount =  floatval(str_replace(',', '', Session::get('checkout')['discount']));
+        $order->tax =  floatval(str_replace(',', '', Session::get('checkout')['tax']));
+        $order->total = floatval(str_replace(',', '', Session::get('checkout')['total']));
+        $order->name = $address->name;
+        $order->phone = $address->phone;
+        $order->locality = $address->locality;
+        $order->address = $address->address;
+        $order->city = $address->city;
+        $order->state = $address->state;
+        $order->country = $address->country;
+        $order->landmark = $address->landmark;
+        $order->zip = $address->zip;
+        $order->save();
+
+        foreach (Cart::instance('cart')->content() as $item) {
+            $orderitem = new OrderItem();
+            $orderitem->product_id = $item->id;
+            $orderitem->order_id = $order->id;
+            $orderitem->price = floatval(str_replace(',', '', $item->price));
+            $orderitem->quantity = $item->qty;
+            $orderitem->save();
+        }
+
+        if ($request->mode == 'card') {
+            # code...
+        } elseif ($request->mode == 'paypal') {
+            # code...
+        } elseif ($request->mode == 'cod') {
+            $transaction = new Transaction();
+            $transaction->user_id = $user_id;
+            $transaction->order_id = $order->id;
+            $transaction->mode = $request->mode;
+            $transaction->status = "pending";
+            $transaction->save();
+        }
+
+        Cart::instance('cart')->destroy();
+        Session::forget('checkout');
+        Session::forget('coupon');
+        Session::forget('discounts');
+        Session::put('order_id', $order->id);
+        return redirect()->route('cart.order.confirmation');
+    }
+
+    public function setAmountForCheckout()
+    {
+        if (!Cart::instance('cart')->count() > 0) {
+            session()->forget('checkout');
+            return;
+        }
+
+        if (session()->has('coupon')) {
+            session()->put('checkout', [
+                'discount' => session()->get('discounts')['discount'],
+                'subtotal' =>  session()->get('discounts')['subtotal'],
+                'tax' =>  session()->get('discounts')['tax'],
+                'total' =>  session()->get('discounts')['total']
+            ]);
+        } else {
+            session()->put('checkout', [
+                'discount' => 0,
+                'subtotal' => Cart::instance('cart')->subtotal(),
+                'tax' => Cart::instance('cart')->tax(),
+                'total' => Cart::instance('cart')->total()
+            ]);
+        }
+    }
+
+    public function order_confirmation()
+    {
+        if (Session::has('order_id')) {
+
+            $order = Order::find(Session::get('order_id'));
+            return view('order-confirmation', compact('order'));
+        }
+        return redirect()->route('cart.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,15 @@ Route::put('/cart/reduce-qunatity/{rowId}', [CartController::class, 'reduce_item
 Route::delete('/cart/remove/{rowId}', [CartController::class, 'remove_item_from_cart'])->name('cart.remove');
 Route::delete('/cart/clear', [CartController::class, 'empty_cart'])->name('cart.empty');
 Route::post('/cart/apply-coupon', [CartController::class, 'apply_coupon_code'])->name('cart.coupon.apply');
-Route::delete('/cart/remove-coupon',[CartController::class,'remove_coupon_code'])->name('cart.coupon.remove');
+Route::delete('/cart/remove-coupon', [CartController::class, 'remove_coupon_code'])->name('cart.coupon.remove');
+
+
+
+Route::get('/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
+Route::post('/place-order', [CartController::class, 'place_order'])->name('cart.place.order');
+Route::get('/order-confirmation', [CartController::class, 'order_confirmation'])->name('cart.order.confirmation');
+
+
 
 Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
 Route::post('/wishlist/add', [WishlistController::class, 'add_to_wishlist'])->name('wishlist.add');
