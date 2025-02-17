59c1ca3042de21ae891b610a11914a1b8e864e76
jatin-fs <jatin.yadav+github@fxis.ai>
2025-02-17 15:06:52 +0530

    Home page Dynamic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -10,7 +12,10 @@ class HomeController extends Controller
 
     public function index()
     {
-        $slides = Slide::where('status',1)->get()->take(3);
-        return view('index',compact('slides'));
+        $slides = Slide::where('status', 1)->get()->take(3);
+        $categories = Category::orderBy('name')->get();
+        $sproducts=Product::whereNotNull('sale_price')->where('sale_price','<>','')->inRandomOrder()->get()->take(8);
+        $fproducts=Product::where('featured','1')->get()->take(8);
+        return view('index', compact('slides', 'categories','sproducts','fproducts'));
     }
 }
